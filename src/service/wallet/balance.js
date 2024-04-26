502b41f0b05d7e5943f09df68b4d1db49eea882e
janexmgd <janexmgd@gmail.com>
2024-04-26 13:19:48 +0700

    removing custom contract address

--- src/service/wallet/balance.js
+++ src/service/wallet/balance.js
@@ -17,30 +17,30 @@ const balanceWallet = async (address, networkReq) => {
     const alchemy = new Alchemy(settings);
     let ethBalance = await alchemy.core.getBalance(address);
     ethBalance = Utils.formatEther(ethBalance);
-    const balances = await alchemy.core.getTokenBalances(address);
-    const nonZeroBalances = balances.tokenBalances.filter((token) => {
-      return token.tokenBalance !== '0';
-    });
-    let tokenList = [];
-    for (let token of nonZeroBalances) {
-      let balance = token.tokenBalance;
-      const metadata = await alchemy.core.getTokenMetadata(
-        token.contractAddress
-      );
-      balance = balance / Math.pow(10, metadata.decimals);
-      const tokenx = {
-        name: metadata.name,
-        balance: balance,
-        symbol: metadata.symbol,
-        ca: token.contractAddress,
-      };
-      tokenList.push(tokenx);
-    }
+    // const balances = await alchemy.core.getTokenBalances(address);
+    // const nonZeroBalances = balances.tokenBalances.filter((token) => {
+    //   return token.tokenBalance !== '0';
+    // });
+    // let tokenList = [];
+    // for (let token of nonZeroBalances) {
+    //   let balance = token.tokenBalance;
+    //   const metadata = await alchemy.core.getTokenMetadata(
+    //     token.contractAddress
+    //   );
+    //   balance = balance / Math.pow(10, metadata.decimals);
+    //   const tokenx = {
+    //     name: metadata.name,
+    //     balance: balance,
+    //     symbol: metadata.symbol,
+    //     ca: token.contractAddress,
+    //   };
+    //   tokenList.push(tokenx);
+    // }
     const data = {
       network: Network.BASE_MAINNET,
       address,
       eth: ethBalance,
-      token: [...tokenList],
+      //   token: [...tokenList],
     };
     return data;
   } catch (error) {
